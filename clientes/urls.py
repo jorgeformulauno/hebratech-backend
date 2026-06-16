11df788a5147129aeb44c16584cf04f147ef2d7a
Jorge Almanza <almanzajorgedaniel96@gmail.com>
2026-06-16 15:36:51 -0500

    Ordenes Clientes Terminadas

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,8 +1,10 @@
+# clientes/urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.cliente_portal, name='cliente_portal'),
     path('registrar/', views.registrar_orden, name='registrar_orden'),
     path('orden-exitosa/<int:idOrden>/', views.orden_exitosa, name='orden_exitosa'),
-    path('mis-ordenes/', views.mis_ordenes, name='mis_ordenes'),
 ]
